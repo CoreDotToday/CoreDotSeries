5287420eb5a5fda42c0d5973be9d280dcc39f2e2
koorukuroo <koorukuroo@gmail.com>
2021-10-12 12:49:31 +0900

    init

---
+++ series/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for Core.Series."""
+
+__author__ = """CoreDotToday"""
+__email__ = 'engine@core.today'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="CoreDotToday",
+    author_email='engine@core.today',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Library for time series",
+    entry_points={
+        'console_scripts': [
+            'series=series.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='series',
+    name='series',
+    packages=find_packages(include=['series', 'series.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/coredottoday/series',
+    version='0.1.0',
+    zip_safe=False,
+)
