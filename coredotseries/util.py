413ac82b1b0a3d2a0c2b3fed0708602495c14b53
koorukuroo <koorukuroo@gmail.com>
2021-11-02 15:14:02 +0900

    Add function comment

--- coredotseries/util.py
+++ coredotseries/util.py
@@ -3,9 +3,23 @@ import warnings
 
 
 def ignore_warning(action='ignore'):
+    """ignore warning message
+
+    Parameter
+    ---------
+    action : str
+        default ignore
+    """
     warnings.filterwarnings(action=action)
 
 
 def plotly_offline_mode(connected=False):
+    """plotly offline mode
+
+    Parameter
+    ---------
+    connected : boolean
+        default False
+    """
     plotly.offline.init_notebook_mode(connected=connected)
 
